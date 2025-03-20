d8fbc9d1993df5f87863f3c5213816a7ecc929ba
Usman Nasir <usman.nasir@kwanso.com>
2025-03-20 11:36:30 +0500

    updates ui

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,12 @@
+export default function Profile() {
+  return (
+    <div className='flex flex-col items-center gap-8 py-10'>
+      <h1 className='h1-bold text-dark100_light900'>All Users</h1>
+      <div className='flex w-full flex-col'>
+        <div className='text-dark200_light800 text-center'>
+          Profile page content coming soon
+        </div>
+      </div>
+    </div>
+  )
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { techMap } from '@/constants'
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const getDevIconClassName = (techName: string) => {
+  const normalizedIconName = techName.replace(/[ .]/g, '').toLowerCase()
+
+  return `${techMap[normalizedIconName]} colored` || 'devicon-devicon-plain'
 }
